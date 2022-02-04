79d9313ba7197dc2727d87fc3fc3f87f023e1326
Taiki Noda <taiki.nd.real@gmail.com>
2022-02-05 00:16:27 +0900

    サーバーの立ち上げ

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment development | production")
+	flag.Parse()
+
+	fmt.Println("run")
+
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "status")
+	})
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
