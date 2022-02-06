6a1011cdb01f47887fe1f6134eb8778636108edd
Taiki Noda <taiki.nd.real@gmail.com>
2022-02-06 23:43:46 +0900

    genre struct

--- models/models.go
+++ models/models.go
@@ -17,6 +17,10 @@ type Movie struct {
 }
 
 type Genre struct {
+	ID        int       `json:"id"`
+	GenreName string    `json:"genre_name "`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type MovieGenre struct {
