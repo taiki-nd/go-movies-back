e01ab9a17b45635cf61299800e637acdf166ceff
Taiki Noda <taiki.nd.real@gmail.com>
2022-02-06 23:41:30 +0900

    movie struct

---
+++ models/models.go
@@ -0,0 +1,23 @@
+package models
+
+import "time"
+
+type Movie struct {
+	ID          int          `json:"id"`
+	Title       string       `json:"title"`
+	Description string       `json:"description"`
+	Year        int          `json:"year"`
+	ReleaseDate time.Time    `json:"release_date"`
+	Runtime     int          `json:"runtime"`
+	Rating      int          `json:"rating"`
+	MPAARating  int          `json":mpaa_rating"`
+	CreatedAt   time.Time    `json:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at"`
+	MovieGenre  []MovieGenre `json:"-"`
+}
+
+type Genre struct {
+}
+
+type MovieGenre struct {
+}
