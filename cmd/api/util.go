ce0a77e94bfcae61f1f4c19114ecfe471d0481f2
Taiki Noda <taiki.nd.real@gmail.com>
2022-02-08 20:59:42 +0900

    Marshalに関する補足

--- cmd/api/util.go
+++ cmd/api/util.go
@@ -10,6 +10,7 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
 	wrapper := make(map[string]interface{})
 	wrapper[wrap] = data
 	js, err := json.Marshal(wrapper)
+	//js, err := json.MarshalIndent(wrapper, "", "\t")で見やすくなる
 	if err != nil {
 		return err
 	}
