04d074ca489eda288d4bad7630801b1b1e7e6f8b
Taiki Noda <taiki.nd.real@gmail.com>
2022-02-08 20:39:35 +0900

    映画詳細ページへルーティング

---
+++ cmd/api/movie-handlers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.logger.Print(errors.New("invalid id parameter "))
+	}
+
+	app.logger.Println("id is", id)
+}
+
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+
+}
