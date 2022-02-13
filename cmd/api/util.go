e8430b730867207e3015d58f4cd42bccfb80132c
Taiki Noda <taiki.nd.real@gmail.com>
2022-02-13 18:13:25 +0900

    出力の形式を変更

--- cmd/api/util.go
+++ cmd/api/util.go
@@ -9,8 +9,8 @@ import (
 func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, wrap string) error {
 	wrapper := make(map[string]interface{})
 	wrapper[wrap] = data
-	js, err := json.Marshal(wrapper)
-	//js, err := json.MarshalIndent(wrapper, "", "\t")で見やすくなる
+	//js, err := json.Marshal(wrapper)
+	js, err := json.MarshalIndent(wrapper, "", "\t") //で見やすくなる
 	if err != nil {
 		return err
 	}
