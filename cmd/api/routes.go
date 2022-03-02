a5c88e2dbe7eacaada01d15db52b255acf9ac112
Taiki Noda <taiki.nd.real@gmail.com>
2022-03-02 23:49:20 +0900

    routeの設定

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
 )
+
+//user認証して編集等を可能にするため
+func (app *application) Wrap(next http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		ctx := context.WithValue(r.Context(), "params", ps)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+}
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+
+	secure := alice.New(app.checkToken)
+
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/signin", app.Signin)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.getOneMovie)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
 	router.HandlerFunc(http.MethodGet, "/v1/genres/:genre_id", app.getAllMoviesByGenre)
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
-	router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editMovie)
+	//router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editMovie)
+	router.POST("/v1/admin/editmovie", app.Wrap(secure.ThenFunc(app.editMovie)))
 	router.HandlerFunc(http.MethodGet, "/v1/admin/deletemovie/:id", app.deleteMovie)
 
 	return app.enableCROS(router)
