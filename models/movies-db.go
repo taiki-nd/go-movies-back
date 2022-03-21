a1f2260634118119a2379223bbad8dd5c52f6767
taiki-nd <taiki.nd.real@gmail.com>
2022-03-21 15:13:37 +0900

    修正

--- models/movies-db.go
+++ models/movies-db.go
@@ -84,7 +84,7 @@ func (m *DBModel) All(genre ...int) ([]*Movie, error) {
 	}
 
 	query := fmt.Sprintf(`select id, title, description, year, release_date, rating, runtime, mpaa_rating,
-				created_at, updated_at from movies  %s order by title`, where)
+				created_at, updated_at, coalesce(poster, '') from movies  %s order by title`, where)
 
 	rows, err := m.DB.QueryContext(ctx, query)
 	if err != nil {
@@ -107,6 +107,7 @@ func (m *DBModel) All(genre ...int) ([]*Movie, error) {
 			&movie.MPAARating,
 			&movie.CreatedAt,
 			&movie.UpdatedAt,
+			&movie.Poster,
 		)
 		if err != nil {
 			return nil, err
